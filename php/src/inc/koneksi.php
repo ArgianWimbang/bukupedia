f43e549eab6cacb3183ce8cb38223956cf4e92a5
Argian13 <argianvarell12@gmail.com>
2021-12-24 18:03:44 +0700

    commit7

--- php/src/inc/koneksi.php
+++ php/src/inc/koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-	$koneksi = mysqli_connect("localhost","root","","toko_buku");
+	$koneksi = mysqli_connect("172.10.0.2","root","admin","toko_buku");
 	 if ($koneksi) {
 	 	echo "Koneksi Berhasil !";
 	}
